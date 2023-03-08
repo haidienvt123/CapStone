b813a9b5759ef8544d2d1ad18c8cf6d63c98ec4f
rtr_intern_haihp <haihp.work@gmail.com>
2023-03-08 21:40:28 +0700

    merge feature 2

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ import serial
 from DL_model import license_id
 
 #read signal from port
-# arduinoData = serial.Serial('COM8',9600)
-# time.delay = 1000
+arduinoData = serial.Serial('/dev/ttyACM0',9600)
+time.delay = 1000
 license_plate=license_id()
 
 # video capture
@@ -49,30 +49,24 @@ class App:
 
         self.live1 = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height*0.8)
         self.live1.grid(row = 0, column = 0, pady = 2)
-        #self.live1.pack()
 
         self.image1 = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height*0.8)
         self.image1.grid(row = 0, column = 1, pady = 2)
-        #self.image1.pack()
 
         self.live2 = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height*0.8)
         self.live2.grid(row = 1, column = 0,pady = 2)
-        #self.live2.pack()
 
         self.image2 = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height*0.8)
         self.image2.grid(row = 1, column = 1, pady = 2)
-        #self.image2.pack()
 
 
         
 
         self.btn_getvideo=tkinter.Button(window, text="takeImage", width=50, command=self.takeImage)
         self.btn_getvideo.grid(row = 2, column = 0, pady = 2)
-        #self.btn_getvideo.pack(anchor=tkinter.CENTER, expand=True)
 
         self.number=tkinter.Button(window, text='0', width=50)
         self.number.grid(row = 2, column = 1, pady = 2)
-        #self.number.pack(anchor=tkinter.CENTER, expand=True)
 
         self.delay = 1
         self.update()
@@ -86,7 +80,7 @@ class App:
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.live1.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
             self.live2.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
-        # self.readUID()
+        self.readUID()
         self.window.after(self.delay, self.update)
     
     #take image and opendoor
@@ -131,5 +125,5 @@ class App:
             self.image_save = PIL.Image.fromarray(frame)
             self.image_save.save("photo/image1.png")
         
-App(tkinter.Tk(), "get_video",0,30)
+App(tkinter.Tk(), "get_video",'/dev/video0',30)
 
