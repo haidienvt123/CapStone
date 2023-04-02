527aa7b9ef2797090fd099b62b1d8934256b4407
rtr_intern_haihp <haihp.work@gmail.com>
2023-04-02 15:26:55 +0700

    test1 - color

--- test1.py
+++ test1.py
@@ -2,10 +2,22 @@ import tkinter
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
 from PIL import Image, ImageTk
-from DL_model import license_id
 import numpy as np
+from tensorflow import keras
+import cv2
 
-license_plate=license_id()
+model = keras.models.load_model('vehicle_color_haze_free_model.h5')
+
+d={'black':0,'blue':1,'cyan':2,'gray':3,'green':4,'red':5,'white':6,'yellow':7}
+d_b={0: 'black',1:'blue',2:'cyan',3:'gray',4:'green',5:'red',6:'white',7:'yellow'}
+
+def take_color(img):
+    dim = (100,100)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    df = resized.astype('float32')
+    df=df/255
+    activations = model.predict(df.reshape(1,100,100,3))
+    return d_b[np.argmax(activations[len(activations)-1])]
 
 class App:
     def __init__(self, window):
@@ -39,11 +51,8 @@ class App:
     
     #take image and opendoor
     def det_img(self):
-        id_str,bbox_image,crop_image=license_plate.license_detect(self.cv_img)
-
-        self.image_show1 = ImageTk.PhotoImage(image = Image.fromarray(bbox_image))
-        self.image1.create_image(0, 0, image = self.image_show1, anchor = tkinter.NW)
-        self.number.config(text=str(id_str[0][0]))
+        self.color = take_color(self.cv_img)
+        self.number.config(text= self.color)
 
     #Read UID card, show on bar and take iamge
     def upload_file(self):
@@ -51,7 +60,7 @@ class App:
         filename = filedialog.askopenfilename(filetypes=f_types)
         self.image=Image.open(filename)
         self.image=self.image.resize((600, 400))
-        self.cv_img = np.array(self.image) 
+        self.cv_img = cv2.imread(filename)
         self.img = ImageTk.PhotoImage(self.image)
         self.live1.create_image(0, 0, image = self.img, anchor = tkinter.NW)
         self.img_path.config(text=filename)
