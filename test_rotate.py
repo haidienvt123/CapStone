c8faae3fc4b3298e883a043b17da403559be4fb5
rtr_intern_haihp <haihp.work@gmail.com>
2023-04-11 16:18:45 +0700

    add bw

--- test_rotate.py
+++ test_rotate.py
@@ -1,16 +1,23 @@
-from PIL import Image
-filename = "lic.jpg"
-
-with Image.open(filename) as img:
-    img.load()
-
-img_gray = img.convert("L")
-threshold = 150
-img_threshold = img_gray.point(
-    lambda x: 255 if x > threshold else 0
-)
-img_threshold.show()
-# threshold = 57
-# img_threshold = blue.point(lambda x: 255 if x > threshold else 0)
-# img_threshold = img_threshold.convert("1")
-# img_threshold.show()
+import cv2
+ 
+image = cv2.imread("lic.jpg")
+gray =cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ret,binary = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)
+contours,hierarchy = cv2.findContours(binary,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+print("Number of contours:" + str(len(contours)))
+min_x = 1000
+min_y = 1000
+max_x = 0
+max_y = 0
+for i in contours:
+    x,y,w,h = cv2.boundingRect(i)
+    if x < min_x:
+        min_x = x
+    if y < min_y:
+        min_y = y
+    if x+w > max_x:
+        max_x = x+w
+    if y+h > max_y:
+        max_y = y+h
+foreground = binary[min_y:max_y, min_x:max_x]
+cv2.imwrite("bw.png",foreground)
